fc9e4aebf3fa0e821654d0cefa71e527964ba10a
THARUN ABBURI <abburitharun88@gmail.com>
2022-08-09 10:32:08 +0000

    removed logout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -77,7 +77,7 @@ const NavBar = () => {
   console.log("---->In the Navbar", finalState, currentValue);
 
   const [isOpen, setIsOpen] = useState(false);
-  const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const { user, isAuthenticated, loginWithRedirect } = useAuth0();
   const toggle = () => setIsOpen(!isOpen);
 
   const logoutWithRedirect = () => (window.location.href = `https://iddev.mcafee.com/logout?redirectTo=${window.location.origin}`);
